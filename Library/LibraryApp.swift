c11a1d6c085bea5c27fbbb28e7dc2a18f932dcef
Ayesha Ilyas <annie16950@gmail.com>
2022-04-16 22:52:39 -0400

    Initial Commit

---
+++ Library/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library
+//
+//  Created by Ayesha Ilyas on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
